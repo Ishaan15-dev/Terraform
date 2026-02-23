b545ea2dad2f32665f88f915df3f70376127bd3e
Ishaan Aggarwal <ishaanaggarwal582@gmail.com>
2026-02-23 11:57:42 +0530

    Delete Management directory

--- Management/NAT-Gateway/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-  bucket  = "otms-app-aptgetswag-bucket"
-   key     = "env/dev/network-skeleton/natgateway/natgateway.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- Management/VPC/bacakend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "otms-app-aptgetswag-bucket"
-    key     = "env/dev/network-skeleton/vpc/vpc.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
